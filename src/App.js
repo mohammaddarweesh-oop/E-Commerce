62701e01d938a57897a26eda008c86571adc8e9b
mohammaddarweesh-oop <githubdarweesh@gmail.com>
2023-05-24 00:10:41 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+// import { BrowserRouter } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>E-Commerce</h1>
+      <Router>
+        <Routes>
+
+          <Route path="/" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/About" element={<About/>}/>
+          <Route path="/faq"/>
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li ><Link>Card</Link> </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
